2c129026a940c8e31a080b93cf60c802ce1058ee
Vybhavi-shetty <165071182+Vybhavi-shetty@users.noreply.github.com>
2025-08-23 10:24:47 +0530

    Add files via upload

--- cie3.c
+++ cie3.c
@@ -1,27 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
+#include <math.h>
+
+int is_prime(int num) {
+    if (num <= 1) return 0;
+    if (num == 2) return 1;
+    if (num % 2 == 0) return 0;
+    for (int i = 3; i <= sqrt(num); i += 2) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
 
 int main() {
-    int A1[6] = {1, 2, 3, 4, 5, 6}; 
-    int A2[6] = {1, 2, 3, 4, 5, 6}; 
+    int n;
+    printf("Enter the upper limit (n): ");
+    scanf("%d", &n);
 
-    
-    #pragma omp parallel for schedule(dynamic, 2)
-    for (int i = 0; i < 6; i++) {
-        A1[i] = 3 * A1[i] + 2;
-        printf("Dynamic: Thread %d processed A1[%d] = %d\n",
-               omp_get_thread_num(), i, A1[i]);
+    if (n <= 2) {
+        printf("Error: There is no prime number less than %d.\n", n);
+        return 1;
     }
 
-    printf("\n");
-
+    int largest_prime_seq = -1;
+    double start_time = omp_get_wtime();
     
-    #pragma omp parallel for schedule(static, 2)
-    for (int i = 0; i < 6; i++) {
-        A2[i] = 3 * A2[i] + 2;
-        printf("Static: Thread %d processed A2[%d] = %d\n",
-               omp_get_thread_num(), i, A2[i]);
+    
+    for (int i = n - 1; i >= 2; i--) {
+        if (is_prime(i)) {
+            largest_prime_seq = i;
+            break;
+        }
     }
 
-    return 0;
+    double time_seq = omp_get_wtime() - start_time;
+    printf("Sequential: Largest prime < %d is %d (Time: %f seconds)\n", n, largest_prime_seq, time_seq);
+
+
+    int largest_prime_par = -1;
+    start_time = omp_get_wtime();
+
+    #pragma omp parallel for
+    for (int i = 2; i < n; i++) {
+        if (is_prime(i)) {
+            #pragma omp critical
+            {
+                if (i > largest_prime_par)
+                    largest_prime_par = i;
+            }
+        }
+    }
+
+    double time_par = omp_get_wtime() - start_time;
+    printf("Parallel: Largest prime < %d is %d (Time: %f seconds)\n", n, largest_prime_par, time_par);
+
+    if (time_par > 0)
+        printf("Speedup: %.2fx\n", time_seq / time_par);
+
+    return 0;
 }
