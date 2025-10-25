42b0198139fd725cc06506ccd23d802e305ff880
Vybhavi-shetty <165071182+Vybhavi-shetty@users.noreply.github.com>
2025-10-25 09:27:46 +0530

    Integrate MPI for inter-process communication

--- cie3.c
+++ cie3.c
@@ -1,63 +1,56 @@
-//Modify the program to find the largest prime number less than n 
+#include <mpi.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <omp.h>
-#include <math.h>
+#include <string.h>
 
-int is_prime(int num) {
-    if (num <= 1) return 0;
-    if (num == 2) return 1;
-    if (num % 2 == 0) return 0;
-    for (int i = 3; i <= sqrt(num); i += 2) {
-        if (num % i == 0) return 0;
+int main(int argc, char* argv[]) {
+    int rank;
+    char msg[50];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        MPI_Recv(msg, 50, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 0 received message: %s\n", msg);
+        strcpy(msg, "Hello from Process 0");
+        MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+    } else if (rank == 1) {
+        MPI_Recv(msg, 50, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 1 received message: %s\n", msg);
+        strcpy(msg, "Hello from Process 1");
+        MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
     }
-    return 1;
+
+    MPI_Finalize();
+    return 0;
 }
 
-int main() {
-    int n;
-    printf("Enter the upper limit (n): ");
-    scanf("%d", &n);
 
-    if (n <= 2) {
-        printf("Error: There is no prime number less than %d.\n", n);
-        return 1;
+
+case 2
+#include <mpi.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    int rank;
+    char msg[50];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) {
+        strcpy(msg, "Hello from Process 0");
+        MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(msg, 50, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 0 received message: %s\n", msg);
+    } else if (rank == 1) {
+        MPI_Recv(msg, 50, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process 1 received message: %s\n", msg);
+        strcpy(msg, "Hello from Process 1");
+        MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
     }
 
-    int largest_prime_seq = -1;
-    double start_time = omp_get_wtime();
-    
-    // Sequential search
-    for (int i = n - 1; i >= 2; i--) {
-        if (is_prime(i)) {
-            largest_prime_seq = i;
-            break;
-        }
-    }
-
-    double time_seq = omp_get_wtime() - start_time;
-    printf("Sequential: Largest prime < %d is %d (Time: %f seconds)\n", n, largest_prime_seq, time_seq);
-
-    // Parallel search
-    int largest_prime_par = -1;
-    start_time = omp_get_wtime();
-
-    #pragma omp parallel for
-    for (int i = 2; i < n; i++) {
-        if (is_prime(i)) {
-            #pragma omp critical
-            {
-                if (i > largest_prime_par)
-                    largest_prime_par = i;
-            }
-        }
-    }
-
-    double time_par = omp_get_wtime() - start_time;
-    printf("Parallel: Largest prime < %d is %d (Time: %f seconds)\n", n, largest_prime_par, time_par);
-
-    if (time_par > 0)
-        printf("Speedup: %.2fx\n", time_seq / time_par);
-
+    MPI_Finalize();
     return 0;
 }
